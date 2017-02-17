596b5bb7e679b7903132735861a8c64be7ad3e16
Alex Forbes-Reed <github@alx.red>
2017-02-17 18:09:58 +0000

    added setmem command

--- main.go
+++ main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"errors"
 	"os"
 
 	"github.com/0xdeafcafe/ares/commands"
+	"github.com/0xdeafcafe/go-xbdm"
+
+	"regexp"
+
+	"fmt"
 
 	"gopkg.in/urfave/cli.v1"
 )
@@ -42,6 +48,53 @@ func main() {
 
 				explorer.StartListening()
 				return explorer.Listen()
+			},
+		},
+		cli.Command{
+			Name:  "memset",
+			Usage: "Explore the Xbox's file system.",
+			Flags: []cli.Flag{
+				cli.Int64Flag{
+					Name:  "address, a",
+					Usage: "The offset in the Xbox's memory to set.",
+				},
+				cli.StringFlag{
+					Name:  "data, d",
+					Usage: "The data to write to the Xbox's memory. Must be a hex formatted byte array.",
+				},
+			},
+			Action: func(c *cli.Context) error {
+				addr := c.Int64("address")
+				data := c.String("data")
+
+				// Validate Addr is within Range
+				if addr < 0 {
+					return errors.New("address can not be negative")
+				}
+
+				// Validate Data is valid hex string
+				regex := regexp.MustCompile("[A-Fa-f0-9]+")
+				if !regex.MatchString(data) {
+					return errors.New("data invalid format")
+				}
+				if len(data)%2 != 0 {
+					return errors.New("data invalid length")
+				}
+
+				// Create connection to xbox
+				xbdm, err := goxbdm.NewXBDMClient(consoleIPAddress)
+				if err != nil {
+					return err
+				}
+
+				// Set memory
+				resp, err := xbdm.SetMemory(addr, data)
+				if err != nil {
+					return err
+				}
+
+				fmt.Println(resp)
+				return nil
 			},
 		},
 	}
